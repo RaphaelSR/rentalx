1aef669c93c9ff21b4f18f2558fb6b13620f6e65
RaphaelSR2 <globalsys.raphael@wine.com.br>
2022-04-02 19:50:49 -0300

    feat: add uuid lib

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const categoriesRoutes = Router();
 
@@ -7,7 +8,8 @@ const categories = [];
 categoriesRoutes.post("/categories", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({ name, description });
+  const category = { name, description, id: uuidv4() };
+  categories.push(category);
 
   return response.status(201).send();
 });
