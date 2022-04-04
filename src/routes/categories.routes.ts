36c924bdc18f33a6523cd3f7fdfb09a0d71dd78e
RaphaelSR2 <globalsys.raphael@wine.com.br>
2022-04-04 10:49:42 -0300

    feat: add service and SRP (SOLID)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/categories", (request, response) => {
   const { name, description } = request.body;
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  console.log({categoryAlreadyExists})
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category Already exists!" });
-  }
-  
-  categoriesRepository.create({ name, description });
   return response.status(201).json();
 });
 
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,19 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface ICreateCategoryServiceRequest {
+  name: string;
+  description: string;
+}
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+  execute({ name, description }: ICreateCategoryServiceRequest) {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
