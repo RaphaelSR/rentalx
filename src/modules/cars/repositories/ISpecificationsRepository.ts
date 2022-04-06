84a9b6189ec85a52852a31aad2bc3b46f2ba79f5
RaphaelSR2 <globalsys.raphael@wine.com.br>
2022-04-06 13:53:12 -0300

    feat: creating specifications repository

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,12 @@
+import { Specification } from "../model/Specification";
+
+interface ICreateISpecificationDTO {
+  name: string;
+  description: string;
+}
+interface ISpecificationsRepository {
+  create({ name, description }: ICreateISpecificationDTO): void;
+  findByName(name: string): Specification;
+}
+
+export { ISpecificationsRepository, ICreateISpecificationDTO };
