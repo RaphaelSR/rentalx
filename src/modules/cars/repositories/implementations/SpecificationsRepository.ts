259d63d0b5bc9fbc8f5aaaca8a265677f09d283e
RaphaelSR2 <globalsys.raphael@wine.com.br>
2022-04-11 11:44:28 -0300

    refactor: separating folders

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,8 +1,8 @@
-import { Specification } from "../model/Specification";
+import { Specification } from "../../model/Specification";
 import {
   ICreateISpecificationDTO,
   ISpecificationsRepository,
-} from "./ISpecificationsRepository";
+} from "../ISpecificationsRepository";
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
 
